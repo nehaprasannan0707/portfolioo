d5a65bfe7dc8038005820b9ba434b17616a355b7
Neha Prasannan <117017344+nehaprasannan0707@users.noreply.github.com>
2025-07-03 13:46:55 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// Hero animation
+ScrollReveal().reveal('.hero-photo', {
+  duration: 1200,
+  origin: 'right',
+  distance: '100px',
+  easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal('.text-content', {
+  duration: 1200,
+  origin: 'left',
+  distance: '100px',
+  delay: 300,
+  easing: 'ease-in-out',
+});
+
+// Section titles and paragraphs
+ScrollReveal().reveal('.content-section', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '50px',
+  easing: 'ease-in-out',
+  interval: 200
+});
+
+// Animate pill skills
+ScrollReveal().reveal('.skill-pill', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '20px',
+  interval: 80,
+  easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal('.contact-link', {
+  duration: 800,
+  origin: 'bottom',
+  distance: '20px',
+  interval: 100,
+  easing: 'ease-in-out'
+});
